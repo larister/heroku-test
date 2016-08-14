1a2661f81ebb351d0a935942a3b2ba9f0f43cb78
Alastair Lockie <alastair@brandwatch.com>
2016-08-14 19:49:52 +0100

    Added dynamic loading of port from env

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var app = require('express')();
 var http = require('http').Server(app);
+require('dotenv').config();
 
 app.get('/', function(req, res){
   res.send('<h1>Hello world</h1>');
 });
 
-http.listen(process.env.$PORT, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT, function(){
+  console.log(`listening on *:${process.env.PORT}`);
 });
 
